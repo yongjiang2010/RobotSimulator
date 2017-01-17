2eb78b918439afe86a4f72edbfd5c9053d491ea5
yongjiang2010 <yongjiang2010@users.noreply.github.com>
2017-01-18 09:52:52 +1300

    Add files via upload

---
+++ Robot.Simulator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Robot.Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Worker worker = new Robot.Bootstrap.Component().Resolve<Worker>();
+
+            worker.Run(); 
+           
+        }
+    }
+
+   
+}
