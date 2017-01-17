32dba118173cde8d109da59680b057e37c1f03ef
yongjiang2010 <yongjiang2010@gmail.com>
2017-01-18 09:12:27 +1300

    Add dependency injection

---
+++ Robot.Bootstrap/Component.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.Unity;
+namespace Robot.Bootstrap
+{
+    public class Component
+    {
+        IUnityContainer _container;
+
+        public Component()
+        {
+            _container = new UnityContainer();
+            Register();
+        }
+
+        private void Register()
+        {
+            _container.RegisterType<RobotContracts.IActionValidator, RobotImplementation.FiveByFiveTableActionValidator>();
+            _container.RegisterType<RobotContracts.IActionable, RobotImplementation.TableRobot>();
+
+        }
+
+        public T Resolve<T>()
+        {
+           return  _container.Resolve<T>();
+        }
+    }
+}
--- RobotSimulator/Program.cs
+++ RobotSimulator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RobotContracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,34 @@ namespace RobotSimulator
     {
         static void Main(string[] args)
         {
+
+            Worker worker = new Robot.Bootstrap.Component().Resolve<Worker>();
+
+            worker.Run();
+           
+        }
+    }
+
+    public class Worker
+    {
+        IActionable _robot;
+
+        public Worker(IActionable robot)
+        {
+            _robot = robot;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Type exit to stop the program...");
+
+            string str = Console.ReadLine();
+            while (!str.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+
+
+                str = Console.ReadLine();
+            }
         }
     }
 }
