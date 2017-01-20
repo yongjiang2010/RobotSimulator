4e32032b89a2ef6c8040eaea8d468a0314e3bd32
yongjiang2010 <yongjiang2010@gmail.com>
2017-01-20 20:55:30 +1300

    Method rename

--- Robot.Tests/FiveByFiveTableActionValidatorUnitTest.cs
+++ Robot.Tests/FiveByFiveTableActionValidatorUnitTest.cs
@@ -9,14 +9,14 @@ namespace Robot.Tests
     public class FiveByFiveTableActionValidatorUnitTest 
     {
         [TestMethod]
-        public void TestIsValidate_1()
+        public void TestValidate_1()
         {
             IActionValidator validator = new FiveByFiveTableActionValidator();
             Assert.IsFalse(validator.Validate(-1, 0), "x is outside the left edge of the table");
         }
 
         [TestMethod]
-        public void TestIsValidate_2()
+        public void TestValidate_2()
         {
             IActionValidator validator = new FiveByFiveTableActionValidator();
             Assert.IsFalse(validator.Validate(5, 0), "x is outside the right edge of the table");
@@ -24,28 +24,28 @@ namespace Robot.Tests
         }
 
         [TestMethod]
-        public void TestIsValidate_3()
+        public void TestValidate_3()
         {
             IActionValidator validator = new FiveByFiveTableActionValidator();
             Assert.IsFalse(validator.Validate(0, -1), "y is outside the bottom edge of the table");
         }
 
         [TestMethod]
-        public void TestIsValidate_4()
+        public void TestValidate_4()
         {
             IActionValidator validator = new FiveByFiveTableActionValidator();
             Assert.IsFalse(validator.Validate(0, 5), "y is outside the top edge of the table");
         }
 
         [TestMethod]
-        public void TestIsValidate_5() 
+        public void TestValidate_5() 
         {
             IActionValidator validator = new FiveByFiveTableActionValidator();
             Assert.IsTrue(validator.Validate(0, 0), "inside table - left bottom corner");
         }
 
         [TestMethod]
-        public void TestIsValidate_6() 
+        public void TestValidate_6() 
         {
             IActionValidator validator = new FiveByFiveTableActionValidator();
             Assert.IsTrue(validator.Validate(4, 4), "inside table - top right corner");
